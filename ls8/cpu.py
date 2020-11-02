434cb6b8dd269e521e7dedc6faad58f4b12d3839
wombat1972 <53923403+wombat1972@users.noreply.github.com>
2020-11-01 19:40:35 -0500

    day 2

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -3,63 +3,76 @@
 
 import sys
 
+LDI = 0b10000010
+PRN = 0b01000111
+HLT = 0b00000001
+MUL = 0b10100010
+
 class CPU:
     """Main CPU class."""
 
     def __init__(self):
         """Construct a new CPU."""
-        # 256-byte RAM, each element is 1 byte (can only store integers 0-255)
+
         self.ram = [0] * 256
-        
-        # R0-R7: 8-bit general purpose registers, R5 = interrupt mask (IM), 
-        # R6 = interrupt status (IS), R7 = stack pointer (SP)
-        self.reg = [0] * 8
+        self.registers = [0] * 8 # R0-R7
+        self.pc = 0 # Program Counter, address of the currently-executing instuction
 
-        # Internal Registers
-        self.pc = 0 # Program Counter: address of the currently executing instruction
-        self.ir = 0 # Instruction Register: contains a copy of the currently executing instruction
-        self.mar = 0 # Memory Address Register: holds the memory address we're reading or writing
-        self.mdr = 0 # Memory Data Register: holds the value to write or the value just read
-        self.fl = 0 # Flag Register: holds the current flags status
-        self.halted = False
+        # "Variables" in hardware. Known as "registers".
+        # There are a fixed number of registers
+        # They have fixed names
+        #  R0, R1, R2, ... , R6, R7
 
-        # Initialize the Stack Pointer
-        # SP points at the value at the top of the stack (most recently pushed), or at address F4 if the stack is empty.
-        self.reg[7] = 0xF4 # 244 # int('F4', 16)
-    
-    def ram_read(self, mar):
-        if mar >= 0 and mar < len(self.ram):
-            return self.ram[mar]
-        else:
-            print(f"Error: Attempted to read from memory address: {mar}, which is outside of the memory bounds.")
-            return -1
+    # accepts the address to read and return the value stored there.
+    def ram_read(self, address):
+        return self.ram[address]
 
-    def ram_write(self, mar, mdr):
-        if mar >= 0 and mar < len(self.ram):
-            self.ram[mar] = mdr & 0xFF
-        else:
-            print(f"Error: Attempted to write to memory address: {mar}, which is outside of the memory bounds.")
+    # accepts a value to write, and the address to write it to
+    def ram_write(self, value, address):
+        self.ram[address] = value
 
     def load(self):
         """Load a program into memory."""
 
-        address = 0
+        try:
+            address = 0
+            with open(sys.argv[1]) as file:
+                for line in file:
+                    split_file = line.split("#")
+                    value = split_file[0].strip()
+                    if value == "":
+                        continue
 
-        # For now, we've just hardcoded a program:
+                    try:
+                        instruction = int(value, 2)
+                    except ValueError:
+                        print(f"Invalid number '{value}'")
+                        sys.exit(1)
 
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
+                    self.ram[address] = instruction
+                    address += 1
 
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+        except FileNotFoundError:
+            print(f"{sys.argv[0]} {sys.argv[1]} file not found")
+            sys.exit()
+
+        # address = 0
+        #
+        # # For now, we've just hardcoded a program:
+        #
+        # program = [
+        #     # From print8.ls8
+        #     0b10000010, # LDI R0,8
+        #     0b00000000,
+        #     0b00001000,
+        #     0b01000111, # PRN R0
+        #     0b00000000,
+        #     0b00000001, # HLT
+        # ]
+        #
+        # for instruction in program:
+        #     self.ram[address] = instruction
+        #     address += 1
 
 
     def alu(self, op, reg_a, reg_b):
@@ -93,30 +106,48 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        running = True
-        while running:
-            # Fetch the next instruction
-            self.ir = self.ram_read(self.pc)
+        self.running = True
+        while self.running:
+            instruction = self.ram_read(self.pc)  # Instruction register, copy of the currently-executing instruction
+
             operand_a = self.ram_read(self.pc + 1)
             operand_b = self.ram_read(self.pc + 2)
 
-            # Decode instruction
-            binary_ir = bin(self.ir)[2:].zfill(8)
-            operand_count = int(binary_ir[:2], 2)
-            is_ALU_operation = binary_ir[2] == '1'
-            instruction_does_set_pc = binary_ir[3] == '1'
-            instruction_id = int(binary_ir[4:], 2)
+            if instruction == HLT: # HLT - halt the CPU and exit the emulator.
+                self.running = False
+                self.pc += 1
+            elif instruction == PRN:
+                print(self.registers[operand_a])
+                self.pc += 2
+            elif instruction == LDI:
+                self.registers[operand_a] = operand_b
+                self.pc += 3
+            elif instruction == MUL:
+                reg_a = self.ram_read(self.pc + 1)
+                reg_b = self.ram_read(self.pc + 2)
+                self.registers[reg_a] = self.registers[reg_a] * self.registers[reg_b]
+                self.pc +=3
 
-            # Increment the program counter
-            self.pc += (1 + operand_count)
 
-            # Execute instruction
-            if self.ir == int('00000001', 2): # HLT
-                running = False
-            elif self.ir == int('10000010', 2): # LDI
-                self.reg[operand_a] = operand_b
-            elif self.ir == int('01000111', 2): # PRN
-                print(self.reg[operand_a])
-            else:
-                print(f"Error: Could not execute instruction: {bin(self.ir)[2:].zfill(8)}")
-                sys.exit(1)
+# Glossary:
+# immediate: takes a constant integer value as an argument
+# register: takes a register number as an argument
+# iiiiiiii: 8-bit immediate value
+# 00000rrr: Register number
+# 00000aaa: Register number
+# 00000bbb: Register number
+# Machine code values shown in both binary and hexadecimal.
+
+# PRN
+# PRN register pseudo-instruction
+# Print numeric value stored in the given register.
+# Print to the console the decimal integer value that is stored in the given register.
+# Machine code:
+# 01000111 00000rrr
+
+# LDI
+# LDI register immediate
+# load "immediate", store a value in a register, or "set this register to this value".
+# Set the value of a register to an integer.
+# Machine code:
+# 10000010 00000rrr iiiiiiii
